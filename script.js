8c8d0e63685e176a64eb522119e47f945b57bbbe
matt keigwin <mkeigwin@bates.edu>
2016-10-04 17:00:35 -0400

    finish line

--- script.js
+++ script.js
@@ -7,40 +7,53 @@ $(document).ready(function() {
   var margintop = $('#player').offset().top;
 
   $('body').on('keyup', movePlayer);
+  $(document).keyup(winner);
 
-function movePlayer(e) {
-  if (e.which===39) {
-    if (marginleft >= ($('window').width())) {
-    marginleft=($('window').width()-(player.width()));
-  } else {
-    marginleft +=10;
-    player.css('left', marginleft + 'px');
+  function movePlayer(e) {
+    if (e.which===39) {
+      if (marginleft >= ($('window').width()-50)) {
+        marginleft=($('window').width()-50);
+      } else {
+        marginleft +=10;
+        player.css('left', marginleft + 'px');
+        //player.attr('src', 'wefawuhfa.png')
+        //just an idea to change character based off of which direction they're facing
+      }
+    }
+    if (e.which===37) {
+      if (marginleft <= 0) {
+        marginleft=0;
+      } else {
+        marginleft -=10;
+        player.css('left', marginleft + 'px');
+      }
+    }
+    if (e.which===40) {
+     if (margintop >= ($('window').height()-50)) {
+        margintop=($('window').height()-50);
+     } else {
+        margintop +=10;
+        player.css('top', margintop + 'px');
+     }
+    }
+    if (e.which===38) {
+      if (margintop <= 0) {
+        margintop=0;
+      } else {
+        margintop -=10;
+        player.css('top', margintop + 'px');
+      }
+    }
   }
+
+ function winner () {
+  // console.log(marginleft, ($(window).width()-200))
+  // if (marginleft >= ($(window).width()-200)) {
+  if ((marginleft >= ($(window).width()-100)) && ((margintop >= (($(window).height())*(4/9))) && (margintop <= (($(window).height())*(5/9))))) {
+    alert("winner");
+    console.log("winner");
   }
-  if (e.which===37) {
-    if (marginleft <= 0) {
-    marginleft=0;
-  } else {
-    marginleft -=10;
-    player.css('left', marginleft + 'px');
-  }
-  }
-  if (e.which===40) {
-     if (margintop >= ($('window').height())) {
-    margintop=($('window').height()-(player.height()));
-} else {
-    margintop +=10;
-    player.css('top', margintop + 'px');
-  }
-}
-  if (e.which===38) {
-    if (margintop <= 0) {
-    margintop=0;
-  } else {
-    margintop -=10;
-    player.css('top', margintop + 'px');
-  }
-}
-}
+ }
+
 
 });
