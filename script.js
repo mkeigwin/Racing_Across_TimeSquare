a5b35f7d1cef7f2b4d83a9c72a8f15d90b49fc58
matt keigwin <mkeigwin@bates.edu>
2016-10-06 11:17:33 -0400

    collide and stop functions

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ console.log('testing javascript');
 $(document).ready(function() {
 
   var player = $('#player');
+  var enemy = $('.random');
   var marginleft = $('#player').offset().left;
   var margintop = $('#player').offset().top;
+  var divnum = $('.random').length;
 
   $('body').on('keyup', movePlayer);
   $(document).keyup(winner);
@@ -48,23 +50,51 @@ $(document).ready(function() {
 
  function winner () {
   if ((marginleft >= ($(window).width()-50)) && ((margintop >= (($(window).height())*(4/9))) && (margintop <= (($(window).height())*(5/9))))) {
-    alert("winner");
+    $('body').off('keyup');
+    $('body').css('background-color', 'purple');
+    $('.random').stop();
   }
  }
 
-function randomdiv () {
-  var divnum = $('.random').length;
-  for (i=0; i < divnum; i++) {
-  var random = $('.random').eq(i);
-  var posleft =random.css('left', (Math.floor(Math.random() * ($(window).width()-50))));
-  var postop =random.css('top', (Math.floor(Math.random() * ($(window).height()-50))));
-  $('.random').eq(i).animate({
-    left: posleft + "px",
-    top:  postop + "px"
-    }, 2000, randomdiv);
- }
-}
- randomdiv ();
+ setInterval(function () {
+    collide(player, enemy);
+  }, 10);
+  setInterval(function () {
+    winner();
+  }, 10);
 
+  function randomdiv () {
+    for (i=0; i < divnum; i++) {
+      var random = $('.random').eq(i);
+      var posleft =random.css('left', (Math.floor(Math.random() * ($(window).width()-50))));
+      var postop =random.css('top', (Math.floor(Math.random() * ($(window).height()-50))));
+      $('.random').eq(i).animate({
+        left: posleft + "px",
+        top:  postop + "px"
+        }, 2000, randomdiv);
+    }
+  }
+   randomdiv ();
+
+  function collide (player, enemy) {
+    for (i=0; i < divnum; i++) {
+      player.top = $(player).offset().top;
+      player.left = $(player).offset().left;
+      player.right = Number($(player).offset().left) + Number($(player).width());
+      player.bottom = Number($(player).offset().top) + Number($(player).height());
+      enemy.top = $(enemy).eq(i).offset().top;
+      enemy.left = $(enemy).eq(i).offset().left;
+      enemy.right = Number($(enemy).eq(i).offset().left) + Number($(enemy).eq(i).width());
+      enemy.bottom = Number($(enemy).eq(i).offset().top) + Number($(enemy).eq(i).height());
+      if (player.right > enemy.left && player.left < enemy.right && player.top < enemy.bottom && player.bottom > enemy.top) {
+          $('body').off('keyup');
+          $('body').css('background-color', 'orange');
+          $('.random').stop();
+       }
+    }
+  }
+  $('body').on('keyup', function() {
+    collide (player, enemy);
+  });
 
 });
